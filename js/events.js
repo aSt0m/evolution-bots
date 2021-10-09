831d155104a4af67d9f9b4edf37274df4417d19a
aSt0m <61126327+aSt0m@users.noreply.github.com>
2021-10-08 22:03:11 -0500

    Add files via upload

--- js/events.js
+++ js/events.js
@@ -1,17 +1,28 @@
-const ham = document.querySelector('.ham');
-const enlaces = document.querySelector('.enlaces-menu');
-const barras = document.querySelectorAll('.ham span');
-// const carrousel = document.querySelector(' .img-carrousel');
+ const ham = document.querySelector('.ham');
+ const enlaces = document.querySelector('.enlaces-menu');
+ const barras = document.querySelectorAll('.ham span');
 
 
 
-ham.addEventListener('click', () => {
-    enlaces.classList.toggle('activado');
-    barras.forEach(child => {
-        child.classList.toggle('animado')});
-    ham.classList.toggle('girar');
-    // carrousel.classList.toggle('none');
-    // text.classList.toggle('none');
-    
-   
+
+ ham.addEventListener('click', () => {
+     enlaces.classList.toggle('activado');
+     barras.forEach(child => {
+         child.classList.toggle('animado')});
+     ham.classList.toggle('girar');  
+ });
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+ loop: true,
 });
